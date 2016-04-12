f14a1656d6ab60285e531b2d78f45bb8e58edb5e
czuchman <czuchman@gmail.com>
2016-04-12 14:37:34 +0200

    Version1

---
+++ Assets/Entities/Player/PlayerAnimation.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+
+	}
+	
+
+	void Update () {
+
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("Right", true);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+            anim.SetBool("Left", false);
+        }
+
+        else anim.SetBool("Right", false);
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("Down", true);
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", false);
+            anim.SetBool("Up", false);
+        }
+
+        else anim.SetBool("Down", false);
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.SetBool("Up", true);
+            anim.SetBool("Down", false);
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", false);
+        }
+
+        else anim.SetBool("Up", false);
+
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Left", true);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+            anim.SetBool("Right", false);
+        }
+
+        else anim.SetBool("Left", false);
+
+        if (Input.GetKeyDown(KeyCode.W) && Input.GetKeyDown(KeyCode.A))
+                {
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", false);
+            anim.SetBool("LeftUp", true);
+        }
+
+       
+
+    }
+}
---
+++ Assets/Entities/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * speed);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed);
+        }
+
+    }
+}
