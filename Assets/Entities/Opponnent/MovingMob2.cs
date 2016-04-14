b30abb2ba3d6a19d0b010d6c2c1992f3d3384d0c
HE201151 <romain.blan@hotmail.fr>
2016-04-14 13:26:30 +0200

    wolf almost ready
    they are going from point A to B , when you go in the area of agro, they
    start following you, if they touch you, you lose health (2sec wait for
    the next attack), just the stop following missing and an animation for
    the attack

---
+++ Assets/Entities/Opponnent/MovingMob2.cs
@@ -0,0 +1,96 @@
+﻿	using UnityEngine;
+	using System.Collections;
+
+public class MovingMob2 : Entity
+{
+
+    public Vector2 farEnd;
+    private Vector2 frometh;
+    private Vector2 untoeth;
+    private float secondsForOneLength = 5f;
+
+    public GameObject target;
+
+    public bool aggro = false;
+
+    bool StartFollow = false;
+    GameObject CollisionTarget;
+
+	public GameObject attackingg;
+	public Entity attacking;
+	public int distance;
+
+
+	private bool canAttack;
+
+    void Start()
+    {
+        farEnd.x = -5;
+        farEnd.y = 2;
+        frometh = transform.position;
+        untoeth = farEnd;
+
+		canAttack = true;
+		if (attackingg == null) {
+			attackingg = GameObject.FindGameObjectWithTag ("Player");
+			attacking = attackingg.GetComponent<Entity>();
+		}
+
+    }
+
+    void Update()
+    {
+        //transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("Player").transform.position, 0.05F);
+        if (StartFollow)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, CollisionTarget.transform.position, 0.05F);
+
+			if (Vector2.Distance (GetComponent<Rigidbody2D>().transform.position, attacking.transform.position) <= distance && canAttack) {
+				attackEntity ();
+				StartCoroutine (waitForAttack ());
+			}
+        }
+        else
+        {
+            transform.position = Vector2.Lerp(frometh, untoeth,
+                Mathf.SmoothStep(0f, 1f,
+                Mathf.PingPong(Time.time / secondsForOneLength, 1f)
+                ));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Collision");
+        aggro = true;
+        // Si le collider en question est celui du joueur
+        if (other.name == "Player")
+        {
+            print("it is a player");
+            CollisionTarget = other.gameObject;
+            StartFollow = true;
+            
+        }
+
+
+
+    }
+
+	public void die(){
+
+	}
+
+	public void attackEntity(){
+		int take = Random.Range (1, 20);
+		attacking.takeHealth (take);
+	}
+
+	IEnumerator waitForAttack(){
+
+		canAttack = false;
+		yield return new WaitForSeconds (2);
+		canAttack = true;
+	}
+}
+
+
