a1e1f767970b216eafe6b74ce53e852021e97d4f
czuchman <czuchman@gmail.com>
2016-05-04 13:26:17 +0200

    Touch controls
    Done touch but HP and Energy need fixing

---
+++ Assets/Entities/Player/PlayerTouchControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerTouchControl : MonoBehaviour
+{ 
+    Rigidbody2D playercharacter;
+    public float playerspeed; 
+
+
+	// Use this for initialization
+	void Start () {
+	
+    playercharacter = this.GetComponent<Rigidbody2D>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector2 movement = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * playerspeed;
+        playercharacter.transform.Translate(movement);
+	}
+}
