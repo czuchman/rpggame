9b7f8712d58165407544b7b601b7274f8ff42c7e
czuchman <czuchman@gmail.com>
2016-05-06 13:19:48 +0200

    Made Start screen
    Simple screen with new game functionality added

---
+++ Assets/Scenes/TitleScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level 0");
+    }
+}
