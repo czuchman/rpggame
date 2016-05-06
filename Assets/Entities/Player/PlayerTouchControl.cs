124d061eba72dad88240d8dccf979105649c99f2
HE201151 <romain.blan@hotmail.fr>
2016-05-06 14:06:56 +0200

    Split script and fix bullet speed problem

--- Assets/Entities/Player/PlayerTouchControl.cs
+++ Assets/Entities/Player/PlayerTouchControl.cs
@@ -10,9 +10,9 @@ public class PlayerTouchControl : Entity
 
 
 	public float speed;
-	public float bulletSpeed;
 
-	public GameObject bullet;
+	public Vector3 pos;
+
 
 	public GameObject EnergyBarr;
 	public Entity EnergyBar;
@@ -20,17 +20,10 @@ public class PlayerTouchControl : Entity
 	public bool isWalking = false;
 	public bool canLoseEnergy = true;
 
-	public string direction;
-	public Vector2 shootinDirection;
 
 
-	//public float speed = 3.0f;
-	private Vector3 targetPos;
-	private Vector3 targetPos2;
 
 
-	public int nbBullet;
-	public Text theText;
 
 
 	// Use this for initialization
@@ -49,11 +42,18 @@ public class PlayerTouchControl : Entity
 	
 	// Update is called once per frame
 	void Update () {
-    	Vector2 movement = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * playerspeed;
-        playercharacter.transform.Translate(movement);
 
+		pos = transform.position;
 
-		theText.text = nbBullet.ToString();
+		if (canMove) {
+			Vector2 movement = new Vector2 (CrossPlatformInputManager.GetAxis ("Horizontal"), CrossPlatformInputManager.GetAxis ("Vertical")) * playerspeed;
+			playercharacter.transform.Translate (movement);
+		}
+
+		if (pos != transform.position) {
+			isWalking = true;
+		}
+
 
 		if (!canMove) {
 			return;
@@ -63,96 +63,18 @@ public class PlayerTouchControl : Entity
 
 
 		if (isWalking && canLoseEnergy) {
-			loseEnergy (1);
+			loseEnergy (2);
 			StartCoroutine (waitForLoseEnergy ());
 			isWalking = false;
 		}
 
 
-		if (Input.GetMouseButtonDown(1) && (nbBullet > 0)){
-
-
-			nbBullet--;
-
-			targetPos2 = Input.mousePosition;
-			targetPos2.z = 0.0f;
-			targetPos2 = Camera.main.ScreenToWorldPoint (targetPos2);
-			targetPos2 = targetPos2 - transform.position;
-
-			GameObject bulletInstance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as GameObject;
-			Rigidbody2D rb2d = bulletInstance.GetComponent<Rigidbody2D> ();
-			rb2d.velocity = new Vector2(targetPos2.x * bulletSpeed, targetPos2.y * bulletSpeed);
-
-
-
-		}
-
-	}
-
-
-	void getDirection(){
-
-		switch (direction){
-
-		case "right":
-			shootinDirection.y = 0;
-			shootinDirection.x = bulletSpeed;
-			break;
-
-		case "down":
-			shootinDirection.y = -bulletSpeed;
-			shootinDirection.x = 0;
-			break;
-
-		case "left":
-			shootinDirection.y = 0;
-			shootinDirection.x = -bulletSpeed;
-			break;
-
-		case "up":
-			shootinDirection.y = bulletSpeed;
-			shootinDirection.x = 0;
-			break;
-
-
-		}
-
 
 
 	}
 
 
-	public void getDirection2(){
 
-		if (targetPos.x > transform.position.x) {
-			shootinDirection.y = 0;
-			shootinDirection.x = bulletSpeed;
-			return;
-		}
-
-		if (targetPos.x < transform.position.x) {
-			shootinDirection.y = 0;
-			shootinDirection.x = -bulletSpeed;
-			return;
-		}
-
-
-		if (targetPos.y < transform.position.y) {
-			shootinDirection.y = -bulletSpeed;
-			shootinDirection.x = 0;
-			return;
-		}
-
-		if (targetPos.y > transform.position.y) {
-			shootinDirection.y = bulletSpeed;
-			shootinDirection.x = 0;
-			return;
-		}
-
-
-
-
-	}
 
 
 	void loseEnergy(int amount){
@@ -170,16 +92,5 @@ public class PlayerTouchControl : Entity
 		canLoseEnergy = true;
 	}
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
 
-		if (other.tag == "PickBullet")
-		{
-			nbBullet++;
-
-		}
-
-
-
-	}
 }
--- Assets/Entities/Player/Shooting.cs
+++ Assets/Entities/Player/Shooting.cs
@@ -1,29 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class Shooting : PlayerFollowMouse {
+public class Shooting : MonoBehaviour {
 
 	public GameObject bullet;
 
-	public float speed;
+	//public float speed;
+
+
+
+	public float bulletSpeed;
+	private Vector3 targetPos2;
+
+	public int nbBullet;
+	public Text theText;
 
 	// Use this for initialization
 	void Start () {
+
+		theText.text = nbBullet.ToString();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		theText.text = nbBullet.ToString();
 	
+		if (Input.GetMouseButtonDown(1) && (nbBullet > 0)){
 
-		if(Input.GetKeyDown (KeyCode.Space) && (nbBullet > 0)){
-			
-			Vector2 position = new Vector2 (-2.0F, 2.0F);
 
-			GameObject obj = Instantiate (bullet, position, Quaternion.identity) as GameObject;
+			nbBullet--;
 
-			nbBullet = nbBullet - 1;
+			targetPos2 = Input.mousePosition;
+			targetPos2.z = 0.0f;
+			targetPos2 = Camera.main.ScreenToWorldPoint (targetPos2);
+			targetPos2 = targetPos2 - transform.position;
+
+			GameObject bulletInstance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as GameObject;
+			Rigidbody2D rb2d = bulletInstance.GetComponent<Rigidbody2D> ();
+			rb2d.velocity = bulletSpeed* (new Vector2(targetPos2.x * bulletSpeed, targetPos2.y * bulletSpeed).normalized);
+
+
+
 		}
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if (other.tag == "PickBullet")
+		{
+			nbBullet++;
+
+		}
+
+
 
 	}
 }
