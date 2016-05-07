155316ea7cc6d5965a15811afaeed7a6c92ca731
czuchman <czuchman@gmail.com>
2016-05-07 13:57:02 +0200

    Added Inventory button
    Some clean ups and scaling problems solved

---
+++ Assets/Animations/InvButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InvButton : MonoBehaviour {
+
+    public GameObject inventory;
+    private Inventory mainInventory;
+
+  
+public void OpenINV()
+    {
+        mainInventory = inventory.GetComponent<Inventory>();
+        mainInventory.openInventory();
+    }
+}
